e2bb5f5630ce755d8766b63ad8325c1533bcd3cc
Kristaliga <checoperez33k@gmail.com>
2025-07-11 15:30:43 -0700

    Add files via upload

---
+++ CONVERSIONPESOSADOLLARS.py
@@ -0,0 +1,7 @@
+# LUIS FERNANDO ARELLANO MARTINEZ 250152
+#PROGRAMA CONVERTIR PESOS A DOLLARS 
+
+n1= float(input("Cantidad en pesos a convertir a dolares: "))
+dll= n1*18.63
+
+print(f"Tus {n1} pesos equivalen a {dll:.2f} dolares")
---
+++ SUMAYDIVISION.py
@@ -0,0 +1,8 @@
+# LUIS FERNANDO ARELLANO MARTINEZ 250152
+#PROGRAMA SUMA Y DIVISION
+
+n= float(input("Cual es tu numero: "))
+suma= n*(n+1)/2
+
+print(f"Tu numero es: {suma}")
+
